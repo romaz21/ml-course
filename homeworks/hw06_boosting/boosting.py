b20b519b06049b03821cf85650e3658008e7e48e
romaz <bmx111roma@mail.ru>
2023-11-01 02:21:14 +0300

    hw6

--- homeworks/hw06_boosting/boosting.py
+++ homeworks/hw06_boosting/boosting.py
@@ -3,34 +3,34 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 
+
 class SimplifiedBoostingRegressor:
     def __init__(self):
         pass
-        
+
     @staticmethod
     def loss(targets, predictions):
-        loss = np.mean((targets - predictions)**2)
+        loss = np.mean((targets - predictions) ** 2)
         return loss
-    
+
     @staticmethod
     def loss_gradients(targets, predictions):
-        gradients = # YOUR CODE HERE
+        gradients = 2 * (targets - predictions)  # YOUR CODE HERE
         assert gradients.shape == targets.shape
         return gradients
-        
-        
+
     def fit(self, model_constructor, data, targets, num_steps=10, lr=0.1, max_depth=5, verbose=False):
         '''
         Fit sequence of models on the provided data.
         Model constructor with no parameters (and with no ()) is passed to this function.
-        If 
-        
+        If
+
         example:
-        
-        boosting_regressor = SimplifiedBoostingRegressor()    
+
+        boosting_regressor = SimplifiedBoostingRegressor()
         boosting_regressor.fit(DecisionTreeRegressor, X, y, 100, 0.5, 10)
         '''
-        new_targets = targets
+        new_targets = lr * targets
         self.models_list = []
         self.lr = lr
         self.loss_log = []
@@ -44,14 +44,16 @@ class SimplifiedBoostingRegressor:
             predictions = self.predict(data)
             self.loss_log.append(self.loss(targets, predictions))
             gradients = self.loss_gradients(targets, predictions)
-            new_targets = # YOUR CODE HERE
+            new_targets = +self.lr * gradients  # YOUR CODE HERE
+            # print(predictions)
         if verbose:
             print('Finished! Loss=', self.loss_log[-1])
         return self
-            
+
     def predict(self, data):
         predictions = np.zeros(len(data))
         for model in self.models_list:
-            predictions += # YOUR CODE HERE
+            predictions += model.predict(data)  # YOUR CODE HERE
+        # print(predictions)
         return predictions
 
